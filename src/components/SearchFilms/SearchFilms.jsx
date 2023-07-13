2c508eb0e051bace1b06c8e19395f94737a54edc
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-07-13 18:12:47 +0200

    +

--- src/components/SearchFilms/SearchFilms.jsx
+++ src/components/SearchFilms/SearchFilms.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import {  useLocation } from 'react-router-dom';
 import { SearchLi, SearchLink, SearchUl } from './SeachFilms.styled';
 
 const SearchFilms = ({ films }) => {
